c3f3e119b3ba1fc0b06b034ce83543e7d4baee3e
Amjad Jibon <amjad.jibon@gmail.com>
2024-12-01 22:02:00 +0800

    add test for Find method in Iterator to handle not found case

--- iterator_test.go
+++ iterator_test.go
@@ -200,3 +200,15 @@ func TestIterator_Find(t *testing.T) {
 	assert.True(t, found)
 	assert.Equal(t, 3, result)
 }
+
+func TestIterator_Find_NotFound(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 5}
+	iter := itertools.ToIter(slice)
+
+	result, found := iter.Find(func(v int) bool {
+		return v == 6
+	})
+
+	assert.False(t, found)
+	assert.Equal(t, 0, result)
+}
